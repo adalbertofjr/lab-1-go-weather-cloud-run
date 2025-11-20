20518e91b632ea6fac1c8219e2630f6c260b0692
Adalberto Fernandes Júnior <adalberto.fernandesjr@gmail.com>
2025-11-20 13:44:31 -0300

    feat(temp): buscando temperatura

--- api/weather_handler.go
+++ api/weather_handler.go
@@ -3,56 +3,106 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
-
-	"github.com/adalbertofjr/lab-1-go-weather-cloud-run/pkg/net"
-	"github.com/adalbertofjr/lab-1-go-weather-cloud-run/pkg/utility"
+	"net/url"
 )
 
 type WeatherHandler struct {
-	CEP CEP
+	Weather Weather
+	APIKey  string
 }
 
-type CEP struct {
-	CEP string `json:"cep"`
+type Weather struct {
+	CEP      string  `json:"cep"`
+	Location string  `json:"localidade"`
+	Current  Current `json:"current"`
 }
 
-func NewWeatherHandler() *WeatherHandler {
-	return &WeatherHandler{}
+type Current struct {
+	Temperature float64 `json:"temp_c"`
 }
 
-func (h *WeatherHandler) GetLocation(w http.ResponseWriter, r *http.Request) {
-	cep := r.URL.Query().Get("cep")
-	if cep == "" {
-		http.Error(w, "CEP is required", http.StatusBadRequest)
-		return
-	}
+func NewWeatherHandler(apikey string) *WeatherHandler {
+	return &WeatherHandler{APIKey: apikey}
+}
 
-	cepValid, err := utility.CEPFormatter(cep)
+func (h *WeatherHandler) GetLocation(cep string) (Weather, error) {
+	url := fmt.Sprintf("http://viacep.com.br/ws/%s/json/", cep)
+	client := http.Client{}
+	resp, err := client.Get(url)
 	if err != nil {
-		http.Error(w, "Invalid CEP format", http.StatusBadRequest)
-		return
+		return Weather{}, err
 	}
 
-	h.CEP = CEP{CEP: cepValid}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return Weather{}, fmt.Errorf("failed to get location data: status code %d", resp.StatusCode)
+	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(h.CEP)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Weather{}, err
+	}
+
+	var weatherData Weather
+	err = json.Unmarshal(body, &weatherData)
+	if err != nil {
+		return Weather{}, err
+	}
+
+	return weatherData, nil
 }
 
-func (h *WeatherHandler) GetCepViaCep(w http.ResponseWriter, r *http.Request) {
-	url := fmt.Sprintf("http://viacep.com.br/ws/%s/json/", "04446160")
-	body := net.FetchData(url, w, r)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(body))
+func (h *WeatherHandler) GetTemperatureCelsius(cep string) (Weather, error) {
+	weather, err := h.GetLocation(cep)
+	if err != nil {
+		return Weather{}, err
+	}
+
+	url := fmt.Sprintf("https://api.weatherapi.com/v1/current.json?key=%s&q=%s&aqi=no", h.APIKey, url.QueryEscape(weather.Location))
+	client := http.Client{}
+	resp, err := client.Get(url)
+	if err != nil {
+		return Weather{}, err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return Weather{}, fmt.Errorf("failed to get location data: status code %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return Weather{}, err
+	}
+
+	// var weatherData Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		return Weather{}, err
+	}
+
+	return weather, nil
 }
 
 func (h *WeatherHandler) GetWeather(w http.ResponseWriter, r *http.Request) {
-	url := fmt.Sprintf("https://api.weatherapi.com/v1/current.json?key=1e5f9bfac6b34555b7c212718251511&q=S%C3%A3o%20Paulo&aqi=no")
-	body := net.FetchData(url, w, r)
+	cep := r.URL.Query().Get("cep")
+
+	weather, err := h.GetTemperatureCelsius(cep)
+	if err != nil {
+		http.Error(w, "Error fetching location", http.StatusInternalServerError)
+		return
+	}
+
+	weatherJSON, err := json.Marshal(weather)
+	if err != nil {
+		http.Error(w, "Error marshalling location data", http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(body))
+	w.Write(weatherJSON)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"github.com/adalbertofjr/lab-1-go-weather-cloud-run/api"
+	"github.com/adalbertofjr/lab-1-go-weather-cloud-run/cmd/configs"
 	"github.com/adalbertofjr/lab-1-go-weather-cloud-run/infra/web"
 )
 
 func main() {
-	startServer()
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	_ = configs
+	startServer(configs)
 }
 
-func startServer() {
+func startServer(configs *configs.Conf) {
 	webserver := web.NewWebServer(":8000")
-	weatherHandler := api.NewWeatherHandler()
+	weatherHandler := api.NewWeatherHandler(configs.WeatherAPIKey)
 	healthHandler := api.NewHealthCheck()
 	webserver.AddHandler("/", weatherHandler.GetWeather)
 	webserver.AddHandler("/health", healthHandler.HealthCheck)
